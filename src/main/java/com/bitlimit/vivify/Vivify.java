9900f989448d8213d7830d41f6f307c626959843
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-09-21 13:48:03 -0700

    Proof of concept complete.

--- src/main/java/com/bitlimit/vivify/Vivify.java
+++ src/main/java/com/bitlimit/vivify/Vivify.java
@@ -1,6 +1,8 @@
 package com.bitlimit.vivify;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Vivify extends JavaPlugin
@@ -9,6 +11,46 @@ public class Vivify extends JavaPlugin
     public void onEnable()
     {
         super.onEnable();
+
+        // Proof of concept
+        String worldName = "world";
+
+        class BlockRunnable implements Runnable
+        {
+            private int currentIndex = 0;
+            private String m_worldName;
+            private Integer[] indices = {80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81};
+
+            public BlockRunnable(String worldName)
+            {
+                this.m_worldName = worldName;
+            }
+
+
+            @Override
+            public void run() {
+
+                currentIndex++;
+
+                if (currentIndex > indices.length - 1)
+                {
+                    currentIndex = 0;
+                }
+
+                for (Integer idx : indices)
+                {
+                    Block block = Bukkit.getWorld(this.m_worldName).getBlockAt(0, idx, 0);
+                    block.setType(Material.AIR);
+                }
+
+                Block animatedBlock = Bukkit.getWorld(this.m_worldName).getBlockAt(0, indices[currentIndex], 0);
+                animatedBlock.setType(Material.BEACON);
+            }
+
+        }
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new BlockRunnable(worldName), 5L, 5L);
+
     }
 
     @Override
